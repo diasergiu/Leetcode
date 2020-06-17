86493103619aa3269bc6f5292ed5f0398094e772
diasergiu <31567436+diasergiu@users.noreply.github.com>
2020-06-17 12:30:39 +0300

    First Problem

---
+++ LeetCode_code_review/Permutation2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_code_review
+{
+    //https://leetcode.com/problems/permutations-ii/
+    class Permutation2
+    {
+        public class Solution
+        {
+
+            IList<IList<int>> permutations;
+
+            public IList<IList<int>> PermuteUnique(int[] nums)
+            {
+                permutations = new List<IList<int>>();
+                FindPermutations(nums, new bool[nums.Length], new List<int>());
+                return permutations;
+
+            }
+
+            private void FindPermutations(int[] nums, bool[] isUsed, List<int> soFar)
+            {
+                if (soFar.Count == nums.Length)
+                {
+                    if (!permutations.Any(x => x.SequenceEqual(soFar)))
+                    {
+                        permutations.Add(soFar.ToList());
+                    }
+                    return;
+                }
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (!isUsed[i])
+                    {
+                        soFar.Add(nums[i]);
+                        isUsed[i] = true;
+                        FindPermutations(nums, isUsed, soFar);
+                        soFar.RemoveAt(soFar.Count - 1);
+                        isUsed[i] = false;
+                    }
+                }
+
+            }
+            private void print(List<int> printList)
+            {
+
+                foreach (int i in printList)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
